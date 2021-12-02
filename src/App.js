d76c7d32560eab558c50073b117b2914f55fbc49
GerhardBiermann <gerhardbiermann2@gmail.com>
2021-12-02 22:40:48 +0200

    SPA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import HomeContent from './components/HomeContent';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
+import { NavLink , Route, HashRouter, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
-      <Navbar/>
+      <div className = "wrapper">
+            <ul>
+                <li className = "menu-items"><NavLink to="/">Home</NavLink></li>
+                <li className = "menu-items"><NavLink to="/projects">Projects</NavLink></li>
+                <li className = "github-linkedin"><a href="https://www.linkedin.com/in/gerhard-biermann-ab868617b/" target="_blank">Linkedin</a></li>
+                <li className = "github-linkedin"><a href="https://github.com/GerhardBiermann" target="_blank">GitHub</a></li>
+                <li className = "menu-items"><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+        </div>
       <Switch>
-        <Route path='/' exact component={HomeContent}/>
+        <Route exact path='/' component={HomeContent}/>
         <Route path='/contact' component={Contact}/>
         <Route path='/projects' component={Projects}/>
       </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
